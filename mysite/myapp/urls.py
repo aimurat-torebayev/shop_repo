ef8367ac01ff6f32d9375fff9fb04f52d93fb2c4
Aimurat Torebayev <kastspy@gmail.com>
2025-01-26 07:57:40 +0500

    Изменение  удаление товара

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from myapp.views import index, indexItem
+from myapp.views import index, indexItem, add_item, update_item, delete_item
 
 app_name = 'myapp'
 
 urlpatterns = [
     # http://127.0.0.1:8000/myapp/ 
-    path('', index),
+    path('', index, name='index'),
     path('<int:productId>/', indexItem, name='detail'),
+    path('additem/', add_item, name='add_item'),
+    path('updateItem/<int:productId>/', update_item, name='update_item'),
+    path('deleteItem/<int:productId>/', delete_item, name='delete_item'),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Product
 
 def index(request):
     items = Product.objects.all()
-    context = {
-        'items':items
-    }
+    context = {'items':items}
     return render(request, 'myapp/index.html', context)
 
 def indexItem(request, productId):
@@ -15,3 +13,38 @@ def indexItem(request, productId):
         'item':item
     }
     return render(request, 'myapp/detail.html', context)
+
+def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        description = request.POST.get('description')
+        image = request.FILES['upload']
+        item = Product(name=name, price=price, description=description, image=image)
+        item.save()
+        
+    return render(request, 'myapp/additem.html')
+
+def update_item(request, productId):
+    item = Product.objects.get(id=productId)
+    
+    if request.method == 'POST':
+        item.name = request.POST.get('name')
+        item.price = request.POST.get('price')
+        item.description = request.POST.get('description')
+        item.image = request.FILES.get('upload', item.image)
+        item.save()
+        return redirect("/myapp/")
+        
+    context = {'item':item}
+    return render(request, 'myapp/updateItem.html', context)
+
+def delete_item(request, productId):
+    item = Product.objects.get(id=productId)
+    
+    if request.method == 'POST':
+        item.delete()
+        return redirect("/myapp/")
+        
+    context = {'item':item}
+    return render(request, 'myapp/deleteItem.html', context)
